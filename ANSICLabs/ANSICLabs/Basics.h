79fddd0dadb6c24413fd72a65f741c668dc8d1c7
Jonathan Lehr <jonathan@aboutobjects.com>
2020-06-08 14:14:50 -0400

    ANSICLabs setup example

---
+++ ANSICLabs/ANSICLabs/Basics.h
@@ -0,0 +1,4 @@
+// Copyright (C) 2020 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#include <stdio.h>
---
+++ ANSICLabs/ANSICLabs/main.m
@@ -0,0 +1,9 @@
+// Copyright (C) 2020 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#import "Basics.h"
+
+int main(int argc, const char * argv[]) {
+    printf("In %s\n", __func__);
+    return 0;
+}
