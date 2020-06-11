1007da606785c37d45042b1f5c64189708f29b4b
Jonathan Lehr <jonathan@aboutobjects.com>
2020-06-11 14:54:26 -0400

    Responder chain demos

--- Coolness/Coolness/CLNAppDelegate.m
+++ Coolness/Coolness/CLNAppDelegate.m
@@ -15,4 +15,20 @@
     [self.window makeKeyAndVisible];
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"In %s", __func__);
+    [UIApplication.sharedApplication sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil];
+}
+
+//- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+//    UITouch *touch = touches.anyObject;
+//    CGPoint currLocation = [touch locationInView:nil];
+//    CGPoint prevLocation = [touch previousLocationInView:nil];
+//    
+//    CGFloat deltaX = currLocation.x - prevLocation.x;
+//    CGFloat deltaY = currLocation.y - prevLocation.y;
+//    
+//    touch.view.frame = CGRectOffset(touch.view.frame, deltaX, deltaY);
+//}
+
 @end
--- Coolness/Coolness/CLNCoolViewController.m
+++ Coolness/Coolness/CLNCoolViewController.m
@@ -75,4 +75,20 @@
     cell2.backgroundColor = UIColor.systemOrangeColor;
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"In %s", __func__);
+    [super touchesBegan:touches withEvent:event];
+}
+
+//- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+//    UITouch *touch = touches.anyObject;
+//    CGPoint currLocation = [touch locationInView:nil];
+//    CGPoint prevLocation = [touch previousLocationInView:nil];
+//    
+//    CGFloat deltaX = currLocation.x - prevLocation.x;
+//    CGFloat deltaY = currLocation.y - prevLocation.y;
+//    
+//    touch.view.frame = CGRectOffset(touch.view.frame, deltaX, deltaY);
+//}
+
 @end
