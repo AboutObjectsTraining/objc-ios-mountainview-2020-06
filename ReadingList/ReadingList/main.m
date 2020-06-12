92308b7116fc93bfc54cc14c2f7702dbfb7f0b4c
Jonathan Lehr <jonathan@aboutobjects.com>
2020-06-11 20:46:14 -0400

    ReadingList app

---
+++ ReadingList/ReadingList/main.m
@@ -0,0 +1,14 @@
+// Copyright (C) 2020 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
